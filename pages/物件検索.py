f124a1036eb3e9339255f906979d855fd674f687
Onodera1218 <onodera.makiko1218@gmail.com>
2023-02-27 10:43:44 +0900

    openpyxlを削除

--- pages/物件検索.py
+++ pages/物件検索.py
@@ -6,16 +6,14 @@ st.markdown('<p style="font-family:HGP創英角ﾎﾟｯﾌﾟ体; color:Salmon;
             unsafe_allow_html=True)
 
 # 選択された築年数でデータフレームをフィルタリング
-df1 = pd.read_csv("muroran_data.csv",engine="openpyxl")
-number1 = st.multiselect('希望のエリアを選択してください',df1['エリア'].unique())
+df1 = pd.read_csv("muroran_data.csv")
+number1 = st.multiselect('希望のエリアを選択してください', df1['エリア'].unique())
 filtered_df1 = df1[df1['エリア'].isin(number1)]
 
-number2 = st.multiselect('希望の築年数を選択してください',filtered_df1['築年数'].unique())
+number2 = st.multiselect('希望の築年数を選択してください', filtered_df1['築年数'].unique())
 filtered_df2 = filtered_df1[filtered_df1['築年数'].isin(number2)]
 
-number3 = st.multiselect('希望の階数を選択してください',filtered_df2['階数'].unique())
+number3 = st.multiselect('希望の階数を選択してください', filtered_df2['階数'].unique())
 filtered_df3 = filtered_df2[filtered_df2['階数'].isin(number3)]
 
 st.table(filtered_df3)
-
-
